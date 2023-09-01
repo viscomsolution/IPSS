0cd6206fc48b41c8f360ee5824b939b1da107954
DESKTOP-BHDQH8H\vohungvi <vohungvi27@gmail.com>
2023-09-01 17:36:15 +0700

    + add example to call x86 to x64

---
+++ IPSScaller/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPSScaller
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch watch;
+
+        string url = "http://localhost:8888/";
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filename = "20151006120042_103.jpg";
+            SendGETrequest(url + filename);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        void SendGETrequest(string request)
+        {
+            watch = Stopwatch.StartNew();
+
+            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
+            {
+                WebRequest wrGETURL;
+                wrGETURL = WebRequest.Create(request);
+
+                WebProxy myProxy = new WebProxy("myproxy", 80);
+                myProxy.BypassProxyOnLocal = true;
+
+                wrGETURL.Proxy = WebProxy.GetDefaultProxy();
+
+                Stream objStream = wrGETURL.GetResponse().GetResponseStream();
+                StreamReader objReader = new StreamReader(objStream);
+
+                string respond = "";
+                string line = "";
+
+                while (line != null)
+                {
+                    line = objReader.ReadLine();
+                    if (line != null)
+                    {
+                        respond = line;
+                        break;
+                    }                        
+                }
+
+                watch.Stop();
+                label1.Text =  respond + " (" + DateTime.Now.ToString("hh:mm:ss") + "   " +  watch.ElapsedMilliseconds.ToString() + "ms)";
+            }
+        }
+    }
+}
---
+++ IPSSlistener/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using TGMT;
+using TGMTcs;
+
+namespace IPSSlistener
+{
+    public partial class Form1 : Form
+    {
+        PlateReader reader;
+        HttpServer server;
+        static Form1 m_instance;
+        Thread t;
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            reader = new PlateReader();
+            server = new HttpServer(8888);
+            server.SetGEThandler(HandleGETRequest);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public static Form1 GetInstance()
+        {
+            if (m_instance == null)
+                m_instance = new Form1();
+            return m_instance;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t = new Thread(new ThreadStart(server.Listen));
+            t.IsBackground = true;
+            t.Start();        
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public static string HandleGETRequest(string GETdata)
+        {
+            string filename = GETdata.Replace("/", "");
+            PlateInfo plate = Form1.GetInstance().reader.Read(filename);
+            return plate.text;
+        }
+    }
+}
