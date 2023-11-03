d9840ff5e68ff573881b62073e80318368bd3af1
vohungvi <vohungvi@vohungvi.com>
2023-11-03 16:53:42 +0700

    + add example

---
+++ ExampleConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TGMT; //TGMT = Cong ty Thi Giac May Tinh
+
+namespace ExampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlateReader reader = new PlateReader();
+
+            PlateInfo plate = reader.Read(@"20151006120042_103.jpg");
+
+            Console.WriteLine(plate.text);
+            Console.WriteLine(plate.alphanumeric);
+
+            Console.ReadLine();
+        }
+    }
+}
